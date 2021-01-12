8b0236f2b6d7699fd00a4a27316180214733e6cd
jeshenoy <jeetendra.shenoy@convorelay.com>
2021-01-12 17:50:56 +0530

    Pojo for Business, BusinessCategory, CreditGroup, Customer
    Pojo for Business, BusinessCategory, CreditGroup, Customer

---
+++ Data-Port-Service/src/main/java/com/dataport/pojo/Business.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.dataport.pojo;
+
+/**
+ * Mapps to Business
+ *
+ */
+public class Business {
+	public String id;
+	public String name;
+	public String phone_number;
+	public String username;
+	public String type;
+	public String description;
+	public String location;
+	public String url;
+	public String photo_url;
+	public String ispublic;
+	public String category_id;
+	public String priority;
+	public String inserted_at;
+	public String updated_at;
+	public String billing_code;
+	public String parent_id;
+}
---
+++ Data-Port-Service/src/main/java/com/dataport/pojo/Customer.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package com.dataport.pojo;
+
+/**
+ * Mapps to user.csv which contains the Customer
+ *
+ */
+public class Customer {
+	public String id;
+	public String name;
+	public String email;
+	public String username;
+	public String phone;
+	public String role;
+	public String mins_paid;
+	public String mins_prepaid;
+	public String mins_used;
+	public String mins_owed;
+	public String mins_updated_at;
+	public String mins_reloaded_at;
+	public String expire_on;
+	public String active;
+	public String inserted_at;
+	public String updated_at;
+	public String password_hash;
+	public String remember_created_at;
+	public String confirmation_token;
+	public String confirmed_at;
+	public String confirmation_sent_at;
+	public String unlock_token;
+	public String failed_attempts;
+	public String locked_at;
+	public String reset_password_token;
+	public String reset_password_sent_at;
+	public String sign_in_count;
+	public String current_sign_in_at;
+	public String last_sign_in_at;
+	public String current_sign_in_ip;
+	public String last_sign_in_ip; 
+	public String phone_extension; 
+	public String age; 
+	public String gender; 
+	public String date_of_birth; 
+	public String photo_url; 
+	public String address_1; 
+	public String address_2; 
+	public String address_3; 
+	public String city; 
+	public String region; 
+	public String postal_code; 
+	public String county; 
+	public String country; 
+	public String gov_id; 
+	public String cellphone; 
+	public String cellphone_operator; 
+	public String mins_type; 
+	public String credit_plan_id; 
+	public String register_type; 
+	public String credit_groups_id; 
+	public String submit_monthly_report; 
+	public String paid_by_hourly;
+
+
+}
