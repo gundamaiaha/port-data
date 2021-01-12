818aadc6273eb59b63bc79b93e3d80974f2e56f2
gundamaiah-convo <gundamaiah.annamaina@convorelay.com>
2021-01-12 15:46:10 +0530

    added csvTOBean code

--- Data-Port-Service/src/main/java/com/dataport/main/DataPortProcessor.java
+++ Data-Port-Service/src/main/java/com/dataport/main/DataPortProcessor.java
@@ -1,8 +1,20 @@
 package com.dataport.main;
 
+import com.dataport.pojo.Agent;
+import com.dataport.service.AgentService;
+
+import java.util.List;
+
 public class DataPortProcessor {
 
     public static void main(String[] args) {
+        
+        //reading agents csv and getting all agent beans
+        List<Agent> agents = new AgentService().buildAgentsFromCsv();
+        System.out.println("agents = " + agents);
+        System.out.println("no of agents = "+agents.size());
+        
+
 
     }
 
