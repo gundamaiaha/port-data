e7f3efc08ed4ebc9059856f85dc767d2987aa9df
jeshenoy <jeetendra.shenoy@convorelay.com>
2021-01-13 14:27:54 +0530

    Update DataPortProcessor
    Update DataPortProcessor

--- Data-Port-Service/src/main/java/com/dataport/main/DataPortProcessor.java
+++ Data-Port-Service/src/main/java/com/dataport/main/DataPortProcessor.java
@@ -16,53 +16,52 @@ import java.util.stream.Collectors;
 
 public class DataPortProcessor {
 
-    public static void main(String[] args) throws IOException {
-        
-    	DataPortProcessor dataPortProcessor = new DataPortProcessor();
-        
+	public static void main(String[] args) throws IOException {
+
+		DataPortProcessor dataPortProcessor = new DataPortProcessor();
+
 //    	dataPortProcessor.generateInterpreterRelatedSQL();
-    	dataPortProcessor.generateCustomerRelatedSQL();
-    	
-    }
-    
-    private void generateInterpreterRelatedSQL() throws IOException {
-        //reading agents csv and getting all agent beans
-        List<Agent> agents = new AgentService().buildAgentsFromCsv();
-        System.out.println("agents = " + agents);
-        System.out.println("no of agents = "+agents.size());
-        
-        InterpreterEntity interpreterEntity = new InterpreterEntity();
-        interpreterEntity.generateInterpreterSql(agents);
+		dataPortProcessor.generateCustomerRelatedSQL();
 
-        InterpreterSpecialityMapEntity interpreterSpecialityMapEntity= new InterpreterSpecialityMapEntity();
-        interpreterSpecialityMapEntity.generateInterpreterSpecialityMap(agents);
+	}
 
-        List<User> users= new UserService().buildUsersFromCsv();
-        System.out.println("users = "+users);
-        System.out.println("no of users = "+users.size());
+	private void generateInterpreterRelatedSQL() throws IOException {
+		// reading agents csv and getting all agent beans
+		List<Agent> agents = new AgentService().buildAgentsFromCsv();
+		System.out.println("agents = " + agents);
+		System.out.println("no of agents = " + agents.size());
 
-        Map<String, String> userNamesMap = agents.stream().collect(Collectors.toMap(Agent::getUserId, Agent::getDescription));
-        UserEntity userEntity= new UserEntity();
-        userEntity.generateUserSqls(users,userNamesMap);
+		InterpreterEntity interpreterEntity = new InterpreterEntity();
+		interpreterEntity.generateInterpreterSql(agents);
 
-        UserRoleMapEntity userRoleMapEntity= new UserRoleMapEntity();
-        userRoleMapEntity.generateUserRoleMapSql(users);
+		InterpreterSpecialityMapEntity interpreterSpecialityMapEntity = new InterpreterSpecialityMapEntity();
+		interpreterSpecialityMapEntity.generateInterpreterSpecialityMap(agents);
 
+		List<User> users = new UserService().buildUsersFromCsv();
+		System.out.println("users = " + users);
+		System.out.println("no of users = " + users.size());
 
+		Map<String, String> userNamesMap = agents.stream()
+				.collect(Collectors.toMap(Agent::getUserId, Agent::getDescription));
+		UserEntity userEntity = new UserEntity();
+		userEntity.generateUserSqls(users, userNamesMap);
 
+		UserRoleMapEntity userRoleMapEntity = new UserRoleMapEntity();
+		userRoleMapEntity.generateUserRoleMapSql(users);
 
-    }
+	}
 
-    private void generateCustomerRelatedSQL() throws IOException {
-        //reading agents csv and getting all agent beans
-        List<Customer> customers = new CustomerService().buildAgentsFromCsv();
-        System.out.println("customers = " + customers);
-        System.out.println("no of customers = " + customers.size());
-        
-        CustomerEntity customerEntity = new CustomerEntity();
-        customerEntity.generateUserAuthDbSql(customers);
+	
+	private void generateCustomerRelatedSQL() throws IOException {
+		// reading agents csv and getting all agent beans
+		List<Customer> customers = new CustomerService().buildAgentsFromCsv();
+//		System.out.println("customers = " + customers);
+		System.out.println("no of customers = " + customers.size());
+
+		CustomerEntity customerEntity = new CustomerEntity();
+		customerEntity.generateUserAuthDbSql(customers);
 
 //        new InterpreterSpecialityMap().generateInterpreterSpecialityMap(agents);
-    }
-    
+	}
+
 }
